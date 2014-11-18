ae04f3d27cb2ec0dcbb19bd0971daab4bbbaed70
Joseph O'Reilly <zebone29@gmail.com>
2014-11-18 16:49:44 +0000

    register and login fixed

--- public/dev/js/app.js
+++ public/dev/js/app.js
@@ -4,7 +4,19 @@ var app = angular.module('coapp', ['ngRoute', 'ui.bootstrap']);
 app.config(function($routeProvider, $httpProvider){
 
 $routeProvider
-	.when('/login')
+	.when('/login', {
+		templateUrl: 'dev/js/views/login.html',
+		//controller: LoginCtrl,
+		access:{
+			requiredLogin: false
+		}
+	}).when('/register',{
+		templateUrl: 'dev/js/views/register.html',
+		//controller: registerCtrl,
+		access:{
+			requiredLogin: false
+		}
+	});
 
 
 });//config
