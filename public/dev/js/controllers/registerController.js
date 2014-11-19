ecbab70ea6ad273853bf2e4abc354897697b5862
Pete Redmond <redmondp@gmail.com>
2014-11-19 11:41:52 +0000

    tidied up files

--- public/dev/js/controllers/registerController.js
+++ public/dev/js/controllers/registerController.js
@@ -1,25 +1,23 @@
 (function(){
-angular.module('coapp').controller('registerController', regController);
+	angular.module('coapp').controller('registerController', regController);
 
-function regController($window, $location, AuthFactory){
+	function regController($window, $location, AuthFactory){
 
-	var _this = this;
+		var _this = this;
 
-	this.register = function(user){
-console.log("register");
-		AuthFactory
-			.register(user)
-			.then(function(data){
-				$location.path('/login');
-			
-			}, function(error){
-				console.log(error);
-			});
-	};
-	
-}
+		this.register = function(user){
+			AuthFactory
+				.register(user)
+				.then(function(data){
+					$location.path('/login');
 
-regController.$inject = ['$window', '$location', 'AuthFactory'];
+				}, function(error){
+					console.log(error);
+				});
+		};
 
+	}
+
+	regController.$inject = ['$window', '$location', 'AuthFactory'];
 
 })();
