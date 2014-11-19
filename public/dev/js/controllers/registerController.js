ecf0131fe76d2e80f91778d64458fc033cd34f28
Joseph O'Reilly <zebone29@gmail.com>
2014-11-19 15:10:32 +0000

    error handling registerCtrl

--- public/dev/js/controllers/registerController.js
+++ public/dev/js/controllers/registerController.js
@@ -2,20 +2,37 @@
 	angular.module('coapp').controller('registerController', regController);
 
 	function regController($window, $location, AuthFactory){
-
+		
 		var _this = this;
 
-	this.register = function(user){
-		AuthFactory
-			.register(user)
-			.then(function(data){
-				$location.path('/login');
-			
-			}, function(error){
-				console.log(error);
-			});
-		}; // register
-	
+		//set up
+		_this.alerts = [];
+		/**
+		 * removes bootstrap alert
+		 * @param  {[type]} index
+		 * @return {[type]}
+		 */		
+		_this.closeAlert = function(index){
+			_this.alerts.splice(index, 1);
+		}
+		this.register = function(user){
+			_this.alerts = [];
+			AuthFactory
+				.register(user)
+				.then(function(data){
+					$location.path('/login');
+				
+				}, function(error){
+					_this.user = {};
+					var err = {
+						type: 'danger',
+						msg: error
+					}
+					_this.alerts.push(err);
+					console.log(error);
+				});
+			}; // register
+		
 	}
 
 	regController.$inject = ['$window', '$location', 'AuthFactory'];
